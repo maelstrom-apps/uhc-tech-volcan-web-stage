258099ca52900765783132d4c3ba3f53b2cdc9a5
john-hollow_uhg <john-hollow_uhg@users.noreply.github.com>
2023-01-18 19:46:31 +0000

    deploy: uhc-tech/volcan@3cbd1431aeb2613a7edb7b39d1faba19da88fe15

--- _next/static/I6JJ1TD24NHYpL0Ihqq0_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/649-ec9dc55103e8ff54.js","static/chunks/pages/index-e3dc5790ff2858f8.js"],"/_error":["static/chunks/pages/_error-a1d5bc3e43249d4c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
