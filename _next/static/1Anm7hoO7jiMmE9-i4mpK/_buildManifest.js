317bd9a6c0aaf0aed40f9c11c5f50d4118c20947
thomas-viancourt_uhg <thomas-viancourt_uhg@users.noreply.github.com>
2023-02-03 16:53:05 +0000

    deploy: uhc-tech/volcan@26274d6376215be789ff6a399a4066441eddc222

---
+++ _next/static/1Anm7hoO7jiMmE9-i4mpK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/67-b29e85771e2c9e4a.js","static/chunks/pages/index-83c759def1268035.js"],"/_error":["static/chunks/pages/_error-1d567a99a7ce80d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
