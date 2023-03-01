43eb751f0cd48b919eb9dbfaa1724f9fe3d640f2
jpunnapu_uhg <jpunnapu_uhg@users.noreply.github.com>
2023-03-01 22:46:36 +0000

    deploy: uhc-tech/volcan@668f8d140350ff1e07521af07e165d5d04c9423b

---
+++ _next/static/8GZSK0rvm1BoyMaoW03bJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/878-be12efd7e838866e.js","static/chunks/pages/index-92875ee4d6b2b095.js"],"/_error":["static/chunks/pages/_error-1d567a99a7ce80d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
