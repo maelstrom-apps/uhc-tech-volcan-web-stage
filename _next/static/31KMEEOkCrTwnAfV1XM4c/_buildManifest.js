19d012a0c99cc3e9f453bb3d61d7b46f9feac556
jwils192_uhg <jwils192_uhg@users.noreply.github.com>
2023-03-08 18:49:23 +0000

    deploy: uhc-tech/volcan@6dcb07cd1cd7e152f5a7154653b85b6ee9bcea27

---
+++ _next/static/31KMEEOkCrTwnAfV1XM4c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/704-ad389c95fe7f00e7.js","static/chunks/pages/index-8fd7f847297ca24c.js"],"/_error":["static/chunks/pages/_error-1d567a99a7ce80d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
